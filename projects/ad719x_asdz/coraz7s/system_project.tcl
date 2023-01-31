15716a5a361ddd63e9899cd7873b9debefb79f13
Iulia Moldovan <Iulia.Moldovan@analog.com>
2023-01-31 13:16:55 +0200

    ad719x_asdz: Add make parameter IO_PMOD_N
    * Made the project to be parameterizable through IO_PMOD_N parameter
    * Added possibility to connect through IO pins also
    
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- projects/ad719x_asdz/coraz7s/system_project.tcl
+++ projects/ad719x_asdz/coraz7s/system_project.tcl
@@ -1,3 +1,11 @@
+set io_mod_n 0
+
+if {[info exists ::env(IO_PMOD_N)]} {
+  set io_pmod_n $::env(IO_PMOD_N)
+} else {
+  set env(IO_PMOD_N) $io_pmod_n
+}
+
 source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
@@ -7,7 +15,7 @@ adi_project ad719x_asdz_coraz7s
 adi_project_files ad719x_asdz_coraz7s [list \
     "$ad_hdl_dir/library/common/ad_iobuf.v" \
     "$ad_hdl_dir/projects/common/coraz7s/coraz7s_system_constr.xdc" \
-    "system_constr.xdc" \
+    "system_constr.tcl" \
     "system_top.v"
 ]
 
