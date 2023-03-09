ef75615ebeacbf655d2348209a7b9d5a676b7343
Iulia Moldovan <Iulia.Moldovan@analog.com>
2023-03-09 19:37:43 +0200

    temp commit. customixe xgui page
    Signed-off-by: Iulia Moldovan <Iulia.Moldovan@analog.com>

--- library/axi_ltc2387/axi_ltc2387_ip.tcl
+++ library/axi_ltc2387/axi_ltc2387_ip.tcl
@@ -8,6 +8,8 @@ source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
 adi_ip_create axi_ltc2387
 
+# ILA instance for ad_datafmt
+
 create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name my_ila_fmt
     set_property -dict [list CONFIG.C_MONITOR_TYPE {Native}] [get_ips my_ila_fmt]
     set_property -dict [list CONFIG.C_NUM_OF_PROBES {7}] [get_ips my_ila_fmt]
@@ -45,13 +47,18 @@ adi_ip_files axi_ltc2387 [list \
 
 adi_ip_properties axi_ltc2387
 
+# customize XGUI layout
+
 ipx::infer_bus_interface ref_clk xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface dco_p xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 ipx::infer_bus_interface dco_n xilinx.com:signal:clock_rtl:1.0 [ipx::current_core]
 
 set cc [ipx::current_core]
-
 set page0 [ipgui::get_pagespec -name "Page 0" -component $cc]
+
+# link to IP documentation
+
+set_property company_url {https://wiki.analog.com/resources/fpga/docs/axi_ltc2387} [ipx::current_core]
 
 ipgui::add_param -name "ADC_RES" -component $cc -parent $page0
 set_property -dict [list \
@@ -76,5 +83,38 @@ set_property -dict [list \
 	"value_validation_list" "16 32" \
  ] \
 [ipx::get_user_parameters OUT_RES -of_objects $cc]
+
+#ipgui::add_param -name "TWOLANES" -component $cc -parent $page0
+#set_property -dict [list \
+#  "display_name" "Configuration mode for lanes" \
+#  "tooltip" "Memory interface" \
+#] [ipgui::get_guiparamspec -name "TWOLANES" -component $cc]
+#
+#set_property -dict [list \
+#	"value_validation_type" "list" \
+#	"value_validation_list" "0 1" \
+# ] \
+#[ipx::get_user_parameters TWOLANES -of_objects $cc]
+
+
+
+
+set_property -dict [list \
+  value_validation_type pairs \
+  value_validation_pairs {ONE-LANE 0 TWO-LANES 1 } \
+] [ipx::get_user_parameters TWOLANES -of_objects $cc]
+
+# if TWOLANES = 0, then activate only DA+/-
+set_property enablement_dependency { $TWOLANES == 0 } \
+  [ipx::get_ports *da* -of_objects $cc]
+
+# if TWOLANES = 1, then activate both ports
+set_property enablement_dependency { $TWOLANES == 1 } \
+  [ipx::get_ports *da* *db* -of_objects $cc]
+#set_property enablement_dependency { $TWOLANES == 1 } \
+#  [ipx::get_ports *db* -of_objects $cc]
+
+#set_property enablement_tcl_expr {$TWOLANES == 1} \
+#  [ipx::get_user_parameters ADC_RES OUT_RES -of_objects $cc]
 
 ipx::save_core [ipx::current_core]
