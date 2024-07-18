spaceinventor/adhdl

axi_dmac: Add framelock, autorun, fsync
The 'framelock' mechanism ensures no buffer is accessed by two DMACs
in the same time.

The core can operate in two modes:
* Writer mode - available in s2mm configuration, the writer DMAC will
                always skip the current in use reader buffer
* Reader mode - available in mm2s configuration, the reader DMAC will
                try to stay behind the writer buffer by either repeating or
                skipping buffers according to the speed relationship of the
                two cores.

The tx and rx DMAC cores must be connected through the dedicated
'framelock' interface. They must be programmed with similar settings
regarding the buffers size, start address and stride.

In 2D mode, the assertion of TLAST on the AXI Stream source interface
signalizes the end of frame, but Xilinx IP cores use this to signalize
then end of line.
Allow to select TLAST to signalize End of Frame and End of Line.

Add external synchronization to be able to throttle the consumption of
descriptors queued by the software.

A transfer will start only after the assertion of the external sync
signal.
The sync signal can be either in source or destination clock domain or
both.
This feature does not ensures fixed latency from the assertion of
external sync signal and the availability of the data at the destination
interface.

In cases where software is not available, the autorun feature can be
used to set up a single transfer that will be executed after reset
deassertion. This is mostly useful together with the CYCLIC mode.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

library/axi_dmac/tb/regmap_tb.v
@@ -57,7 +57,7 @@ module regmap_tb;
   localparam VAL_DBG_IDS0 = 32'h01234567;
   localparam VAL_DBG_IDS1 = 32'h89abcdef;
 
-  localparam AW = 11;
+  localparam AW = 12;
   localparam NUM_REGS = 'h200;
 
   wire s_axi_aclk = clk;
@@ -169,9 +169,9 @@ module regmap_tb;
     for (i = 0; i < NUM_REGS; i = i + 1)
       expected_reg_mem[i] <= 'h00;
     /* Non zero power-on-reset values */
-    set_reset_reg_value('h00, 32'h00040361); /* PCORE version register */
+    set_reset_reg_value('h00, 32'h00040562); /* PCORE version register */
     set_reset_reg_value('h0c, 32'h444d4143); /* PCORE magic register */
-    set_reset_reg_value('h10, 32'h00002101); /* Interface Description*/
+    set_reset_reg_value('h10, 32'h40072101); /* Interface Description*/
     set_reset_reg_value('h80, 'h3); /* IRQ mask */
 
     set_reset_reg_value('h40c, 'h3); /* Flags */
@@ -234,7 +234,7 @@ module regmap_tb;
 
   reg response_eot = 1'b0;
 
-  wire [6:0] response_measured_burst_length = 'hff;
+  wire [7:0] response_measured_burst_length = 'hff;
   wire ctrl_enable;
   wire ctrl_pause;
   wire request_valid;

zedlovegenshin/zzz

library/axi_dmac/tb/regmap_tb.v
@@ -57,7 +57,7 @@ module regmap_tb;
   localparam VAL_DBG_IDS0 = 32'h01234567;
   localparam VAL_DBG_IDS1 = 32'h89abcdef;
 
-  localparam AW = 11;
+  localparam AW = 12;
   localparam NUM_REGS = 'h200;
 
   wire s_axi_aclk = clk;
@@ -169,9 +169,9 @@ module regmap_tb;
     for (i = 0; i < NUM_REGS; i = i + 1)
       expected_reg_mem[i] <= 'h00;
     /* Non zero power-on-reset values */
-    set_reset_reg_value('h00, 32'h00040563); /* PCORE version register */
+    set_reset_reg_value('h00, 32'h00040564); /* PCORE version register */
     set_reset_reg_value('h0c, 32'h444d4143); /* PCORE magic register */
-    set_reset_reg_value('h10, 32'h00002101); /* Interface Description*/
+    set_reset_reg_value('h10, 32'h40072101); /* Interface Description*/
     set_reset_reg_value('h80, 'h3); /* IRQ mask */
 
     set_reset_reg_value('h40c, 'h3); /* Flags */
@@ -234,7 +234,7 @@ module regmap_tb;
 
   reg response_eot = 1'b0;
 
-  wire [6:0] response_measured_burst_length = 'hff;
+  wire [7:0] response_measured_burst_length = 'hff;
   wire ctrl_enable;
   wire ctrl_pause;
   wire request_valid;