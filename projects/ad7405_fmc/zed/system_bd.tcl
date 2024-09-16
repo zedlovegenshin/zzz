7fc083c4e4f07f3fffc6507c403b1f425aca1b21
PopPaul2021 <paul.pop@analog.com>
2024-09-16 17:10:07 +0300

    ad7405: Updates after review
    Signed-off-by: PopPaul2021 <paul.pop@analog.com>

--- projects/ad7405_fmc/zed/system_bd.tcl
+++ projects/ad7405_fmc/zed/system_bd.tcl
@@ -14,12 +14,12 @@ set sys_cpu_clk_freq  100
 # system clock
 # NOTE: For '7 Series' FPGAs the FVCO must be between 600 MHz and 12000 MHz
 
-set clkgen_vco_div 5
-set clkgen_vco_mul 50
+set clkgen_vco_div 1
+set clkgen_vco_mul 10
 
 # specify the external clock rate in MHz (MCLKIN)
 
-set ext_clk_rate 25
+set ext_clk_rate 20
 
 source ../common/ad7405_bd.tcl
 
